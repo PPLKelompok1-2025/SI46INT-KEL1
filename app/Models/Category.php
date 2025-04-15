532df59283c36c00bbcabbff00280157795505ad
baiqueee <hughdev101@gmail.com>
2025-04-15 15:50:51 +0700

    PPL2025-43, PPL2025-44, PPL2025-45 feat: Category Browse Pages

---
+++ app/Models/Category.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'parent_id',
+        'icon',
+    ];
+
+    public function courses()
+    {
+        return $this->hasMany(Course::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+}
